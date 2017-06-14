1ce3563b84e89f50eecb5a5d1d299d2e8f96c255
myIOS <973490661@qq.com>
2017-06-14 22:32:55 +0800

    搭建Tab

---
+++ DFishTV/DFishTV/Classes/Main/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  DFishTV
+//
+//  Created by 张海勇 on 2017/6/14.
+//  Copyright © 2017年 张海勇. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        addChildVc(storyName: "Home")
+        addChildVc(storyName: "Live")
+        addChildVc(storyName: "Follow")
+        addChildVc(storyName: "Profile")
+        
+    }
+    
+    private func addChildVc(storyName : String) {
+    
+        let childVc = UIStoryboard.init(name: storyName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(childVc)
+    }
+
+
+
+}
